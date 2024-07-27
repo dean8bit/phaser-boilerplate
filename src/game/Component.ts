25e1cf00b2bce4668d6f4b1a98065d104fbddd87
Dean R <platoisalive@outlook.com>
2024-07-27 09:34:23 +0000

    updates

--- src/game/Component.ts
+++ src/game/Component.ts
@@ -1,87 +1,111 @@
-export default class Component {
-	public readonly gameObject: Phaser.GameObjects.GameObject;
-	private readonly componentReference: string;
-	constructor(
-		gameobject: Phaser.GameObjects.GameObject,
-		componentReference: string
-	) {
-		this.gameObject = gameobject;
-		this.componentReference = componentReference;
-		// eslint-disable-next-line @typescript-eslint/no-explicit-any
-		(this.gameObject as any)[this.componentReference] = this;
-		this.gameObject.scene.events.once(
-			Phaser.Scenes.Events.UPDATE,
-			this.tryOnStart,
-			this
-		);
-		this.gameObject.scene.events.on(
-			Phaser.Scenes.Events.UPDATE,
-			this.tryOnUpdate,
-			this
-		);
-		this.gameObject.on(
-			Phaser.GameObjects.Events.DESTROY,
-			this.tryOnDestroy,
-			this
-		);
-	}
+import "phaser";
 
-	// eslint-disable-next-line @typescript-eslint/no-empty-function
-	public onStart(): void {}
+export default class Component<T extends Phaser.GameObjects.GameObject> {
+  public readonly gameObject: T;
 
-	private tryOnStart(): void {
-		this.tryCall(this.onStart);
-	}
+  public static getComponents<
+    T extends Component<Phaser.GameObjects.GameObject>
+  >(
+    gameObject: Phaser.GameObjects.GameObject,
+    typeConstructor: new (...args: any[]) => T
+  ): T[] {
+    return (gameObject as any).components
+      ? ((
+          (gameObject as any)
+            .components as Component<Phaser.GameObjects.GameObject>[]
+        ).filter((c) => c instanceof typeConstructor) as T[])
+      : [];
+  }
 
-	// eslint-disable-next-line @typescript-eslint/no-empty-function
-	public onUpdate(): void {}
+  constructor(gameObject: T, updateHook: boolean) {
+    this.gameObject = gameObject;
+    this.init();
+    this.add();
+    this.hooks(updateHook);
+  }
 
-	private tryOnUpdate(): void {
-		this.tryCall(this.onUpdate);
-	}
+  private init() {
+    if (!(this.gameObject as any).components) {
+      (this.gameObject as any).components = [];
+    }
+  }
 
-	// eslint-disable-next-line @typescript-eslint/no-empty-function
-	public onDestroy(): void {}
+  private add() {
+    (this.gameObject as any).components.push(this);
+  }
 
-	private tryOnDestroy(): void {
-		this.gameObject.scene.events.off(
-			Phaser.Scenes.Events.UPDATE,
-			this.tryOnStart,
-			this
-		);
-		this.gameObject.scene.events.off(
-			Phaser.Scenes.Events.UPDATE,
-			this.tryOnUpdate,
-			this
-		);
-		this.gameObject.off(
-			Phaser.GameObjects.Events.DESTROY,
-			this.tryOnDestroy,
-			this
-		);
-		this.tryCall(this.onDestroy);
-		// eslint-disable-next-line @typescript-eslint/no-explicit-any
-		delete (this.gameObject as any)[this.componentReference];
-	}
+  private hooks(updateHook: boolean) {
+    this.gameObject.scene.events.once(
+      Phaser.Scenes.Events.UPDATE,
+      this.tryOnStart,
+      this
+    );
 
-	public destroy(): void {
-		this.tryOnDestroy();
-	}
+    if (updateHook)
+      this.gameObject.scene.events.on(
+        Phaser.Scenes.Events.UPDATE,
+        this.tryOnUpdate,
+        this
+      );
 
-	// eslint-disable-next-line @typescript-eslint/ban-types
-	private tryCall(f: () => void) {
-		try {
-			f.bind(this)();
-			// eslint-disable-next-line @typescript-eslint/no-explicit-any
-		} catch (e: any) {
-			console.log(
-				"(" +
-					this.gameObject.name +
-					":" +
-					this.componentReference +
-					") Error: " +
-					e.stack
-			);
-		}
-	}
+    this.gameObject.on(
+      Phaser.GameObjects.Events.DESTROY,
+      this.tryOnDestroy,
+      this
+    );
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  public onStart(): void {}
+
+  private tryOnStart(): void {
+    this.tryCall(this.onStart);
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  public onUpdate(): void {}
+
+  private tryOnUpdate(): void {
+    this.tryCall(this.onUpdate);
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  public onDestroy(): void {}
+
+  private tryOnDestroy(): void {
+    this.gameObject.scene.events.off(
+      Phaser.Scenes.Events.UPDATE,
+      this.tryOnStart,
+      this
+    );
+    this.gameObject.scene.events.off(
+      Phaser.Scenes.Events.UPDATE,
+      this.tryOnUpdate,
+      this
+    );
+    this.gameObject.off(
+      Phaser.GameObjects.Events.DESTROY,
+      this.tryOnDestroy,
+      this
+    );
+    this.tryCall(this.onDestroy);
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const components = (this.gameObject as any).components;
+    const index = components.indexOf(this);
+    if (index !== -1) {
+      components.splice(index, 1);
+    }
+  }
+
+  public destroy(): void {
+    this.tryOnDestroy();
+  }
+
+  private tryCall(f: () => void) {
+    try {
+      f.call(this);
+    } catch (error: any) {
+      console.error(`(${this.gameObject.name}) ERROR: ${error.stack}`);
+    }
+  }
 }
--- src/game/components/Test.ts
+++ src/game/components/Test.ts
@@ -1,9 +1,9 @@
 import Component from "../Component";
 
 export default class Test extends Component {
-	public onStart(): void {}
+  public onStart(): void {}
 
-	public onUpdate(): void {
-		console.log("update");
-	}
+  public onUpdate(): void {
+    console.log("update");
+  }
 }
